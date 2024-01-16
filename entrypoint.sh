9c26c80471ce4aa58b1eb0876f7e1e71059b63ff
Kostia <kostia3507@gmail.com>
2024-01-16 23:08:52 +0200

    added entry point bash file

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# Perform Django database migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# Start Django server
+#python manage.py runserver 0.0.0.0:8000
+
+#daphne config.asgi:application - websockets
