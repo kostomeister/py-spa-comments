454091886b8dfffe8f6d75f679d537236d84cb3b
Kostia <kostia3507@gmail.com>
2024-01-16 18:20:11 +0200

    implemented settings for email sending

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,10 @@ REST_CAPTCHA = {
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("SENDER_EMAIL")
+EMAIL_HOST_PASSWORD = os.environ.get("SENDER_EMAIL_PASSWORD")
