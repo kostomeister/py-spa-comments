859939830ed3ff51f4c6cbec57aba0d40042a4fd
Kostia <kostia3507@gmail.com>
2024-01-16 23:07:51 +0200

    fixed settings file

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)]
+            "hosts": [("redis", 6379)]
         }
     }
 }
@@ -153,8 +153,8 @@ REST_CAPTCHA = {
     "NOISE_FUNCTION": "rest_captcha.captcha.noise_default",
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
